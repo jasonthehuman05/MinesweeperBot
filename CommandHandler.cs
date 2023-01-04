a5d05364a34ee4e1e42cbb06f0ea9a5b6147ed95
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-01-04 12:07:08 +0000

    Created command handler Inserted the code to handle the Discord bot and its connection to Discord

---
+++ CommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperBot
+{
+    internal class CommandHandler
+    {
+        public CommandHandler()
+        {
+
+        }
+
+        public async Task<SlashCommandProperties> LoadCommands()
+        {
+            //Creates the command and assigns the informaiton needed
+            SlashCommandBuilder commandBuilder = new SlashCommandBuilder();
+
+            commandBuilder.WithName("minesweeper");
+            commandBuilder.WithDescription("Generate a minesweeper board");
+            
+
+            //Builds it to be added
+            SlashCommandProperties scp = commandBuilder.Build();
+
+            return scp;
+        }
+
+        public async Task<Task> CommandExecuted(SocketSlashCommand arg)
+        {
+            await DoAction(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task<Task> DoAction(SocketSlashCommand arg)
+        {
+            arg.DeferAsync(); //Tell discord the command has been received
+
+
+            await arg.FollowupAsync("Received"); //Send reply
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,9 +5,56 @@ namespace MinesweeperBot
 {
     internal class Program
     {
+        public static DiscordSocketClient client;
+        static string token = File.ReadAllText("token.txt");
+
+        static CommandHandler commandHandler;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Minesweeper Bot");
+            Console.WriteLine("Starting Bot...");
+
+            //generate command handler
+            commandHandler = new CommandHandler();
+            MainAsyncProcess();
+            while (true) ;
+        }
+
+        static async void MainAsyncProcess()
+        {
+            //Create the discord client and wire up all needed events
+            client = new DiscordSocketClient();
+            client.Log += DiscordLog;
+            client.Ready += BotReady;
+            
+            //Create event to process command
+            client.SlashCommandExecuted += commandHandler.CommandExecuted;
+
+            //Attempt first log in
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            //Do nothing until program is closed
+            await Task.Delay(-1);
+            await client.StopAsync();
+            client.Dispose();
+        }
+
+        private static async Task BotReady() //When bot is ready, run the command loader
+        {
+            Console.WriteLine("Bot Ready!");
+
+            SlashCommandProperties scp = await commandHandler.LoadCommands();
+            await client.CreateGlobalApplicationCommandAsync(scp);
+
+            Console.WriteLine("Command Registered!");
+        }
+
+        private static Task DiscordLog(LogMessage arg) //Log any messages from the discord gateway
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
