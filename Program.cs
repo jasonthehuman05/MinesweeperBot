bd3f6c9440872f3d248c4c3264efa03af2d98756
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-01-04 11:52:11 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace MinesweeperBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minesweeper Bot");
+        }
+    }
+}
